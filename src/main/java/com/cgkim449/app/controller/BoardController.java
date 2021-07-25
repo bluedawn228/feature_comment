d784cd469a1b00b23675f8bbf4dff429d35a70b9
cgkim449 <cgkim449@gmail.com>
2021-07-26 01:29:38 +0900

    feat: 조회, 수정 페이지에서 다시 목록 페이지로 돌아갈 때 사용자가 기존에 보던 페이지 번호로 이동

--- src/main/java/com/cgkim449/app/controller/BoardController.java
+++ src/main/java/com/cgkim449/app/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,7 +46,7 @@ public class BoardController {
   }
 
   @GetMapping({"/get", "/modify"})
-  public void get(@RequestParam("bno") Long bno, Model model) {
+  public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
     model.addAttribute("board", service.get(bno));
   }
 
@@ -61,18 +62,22 @@ public class BoardController {
 
 
   @PostMapping("/modify")
-  public String modify(BoardVO board, RedirectAttributes rttr) {
+  public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
     if(service.modify(board)) {
       rttr.addFlashAttribute("result", "success");
     }
+    rttr.addAttribute("pageNum", cri.getPageNum());
+    rttr.addAttribute("amount", cri.getAmount());
     return "redirect:/board/list";
   }
 
   @PostMapping("/remove")
-  public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+  public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
     if(service.remove(bno)) {
       rttr.addFlashAttribute("result", "success");
     }
+    rttr.addAttribute("pageNum", cri.getPageNum());
+    rttr.addAttribute("amount", cri.getAmount());
     return "redirect:/board/list";
   }
 }
