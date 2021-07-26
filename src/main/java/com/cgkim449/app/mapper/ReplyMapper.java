67e78260c6ea3adf9b87bcfa57620e4cec13bcb7
cgkim449 <cgkim449@gmail.com>
2021-07-26 14:12:56 +0900

    feat: 영속 계층의 댓글 CRUD 구현

---
+++ src/main/java/com/cgkim449/app/mapper/ReplyMapper.java
@@ -0,0 +1,18 @@
+package com.cgkim449.app.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.cgkim449.app.domain.Criteria;
+import com.cgkim449.app.domain.ReplyVO;
+
+public interface ReplyMapper {
+  int insert(ReplyVO vo);
+  ReplyVO read(Long rno); // 특정 댓글 읽기
+  int delete(int rno);
+  int update(ReplyVO reply);
+
+  public List<ReplyVO> getListWithPaging(
+      @Param("cri") Criteria cri,
+      @Param("bno") Long bno);
+  // Criteria와, 그리고 추가적으로 해당 게시물의 번호가 필요하다
+}
