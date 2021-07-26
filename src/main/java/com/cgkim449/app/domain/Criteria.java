a64ea432f1d9731a30be0d5ea30a859daec53009
cgkim449 <cgkim449@gmail.com>
2021-07-26 11:44:22 +0900

    feat: UriComponentsBuilder를 이용하여 url 생성

--- src/main/java/com/cgkim449/app/domain/Criteria.java
+++ src/main/java/com/cgkim449/app/domain/Criteria.java
@@ -1,5 +1,6 @@
 package com.cgkim449.app.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -29,5 +30,14 @@ public class Criteria {
   // getTypeArr()을 이용해서 MyBatis의 동적 태그를 활용하자
   public String[] getTypeArr() {
     return type == null ? new String[] {} : type.split("");
+  }
+
+  public String getListLink() {
+    UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+        .queryParam("pageNum", this.getPageNum())
+        .queryParam("amount", this.getAmount())
+        .queryParam("type", this.getType())
+        .queryParam("keyword", this.getKeyword());
+    return builder.toUriString();
   }
 }
