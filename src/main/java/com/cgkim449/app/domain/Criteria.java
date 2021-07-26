bae706b83c5dd50bf463e5b290b36776837df3f2
cgkim449 <cgkim449@gmail.com>
2021-07-26 10:21:49 +0900

    feat: MyBatis, Spring에서 검색 조건 처리

--- src/main/java/com/cgkim449/app/domain/Criteria.java
+++ src/main/java/com/cgkim449/app/domain/Criteria.java
@@ -13,6 +13,9 @@ public class Criteria {
   private int pageNum; // 현재 페이지 번호
   private int amount; // 한 페이지당 보여줄 데이터의 개수
 
+  private String type;
+  private String keyword;
+
   public Criteria() {
     this(1, 10); // 기본값을 1 페이지, 10 amount로 하자
   }
@@ -20,5 +23,11 @@ public class Criteria {
   public Criteria(int pageNum, int amount) {
     this.pageNum = pageNum;
     this.amount = amount;
+  }
+
+  // 검색 조건이 T, W, C로 구성되어있으므로 검색 조건을 배열로 만들어서 한번에 처리하기 위함
+  // getTypeArr()을 이용해서 MyBatis의 동적 태그를 활용하자
+  public String[] getTypeArr() {
+    return type == null ? new String[] {} : type.split("");
   }
 }
